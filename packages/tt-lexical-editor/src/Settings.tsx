de8475afbf29f1324c92e78af617e9e4b92ae6ed
Said Akhmedbayev <said.akh@gmail.com>
2024-02-19 19:34:23 -0500

    fixed app settings context

--- packages/tt-lexical-editor/src/Settings.tsx
+++ packages/tt-lexical-editor/src/Settings.tsx
@@ -8,9 +8,9 @@
 
 import * as React from 'react';
 import {useMemo, useState} from 'react';
+import {useSettings} from 'tt-lexical/src/context/SettingsContext';
 
 import {isDevPlayground} from './appSettings';
-import {useSettings} from './context/SettingsContext';
 import Switch from './ui/Switch';
 
 export default function Settings(): JSX.Element {
