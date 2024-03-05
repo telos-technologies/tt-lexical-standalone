25cf305b3ee55b70e94ee8c65df5b4a402f60725
Said Akhmedbayev <said.akh@gmail.com>
2024-03-05 14:15:12 -0500

    removed header styles

--- packages/lexical-standalone-editor/dist/Editor.js
+++ packages/lexical-standalone-editor/dist/Editor.js
@@ -21,7 +21,7 @@ require('@lexical/react/LexicalRichTextPlugin');
 require('@lexical/react/LexicalTabIndentationPlugin');
 require('@lexical/react/LexicalTablePlugin');
 require('@lexical/react/useLexicalEditable');
-var Editor = require('./Editor-eb6ebc68.js');
+var Editor = require('./Editor-f26a1586.js');
 require('react');
 require('@lexical/code');
 require('@lexical/link');
